62fef046e0116d731b35dd99b9625ab7c9dffbc4
Patrick Hillert <phillert@inovex.de>
2018-04-30 11:10:40 +0200

    6): Register addons in Storybook and set basic config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
 import { configure } from '@storybook/vue';
+import { setOptions } from '@storybook/addon-options';
+
+// options
+setOptions({
+  name: 'my elements',
+  url: '/'
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /.stories.js$/);
