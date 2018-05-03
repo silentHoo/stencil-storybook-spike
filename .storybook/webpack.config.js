e530086367f542bf0f6cd1383ef7324fb4eca805
Patrick Hillert <phillert@inovex.de>
2018-05-03 16:56:02 +0200

    8) Extend the Storybook Webpack config

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (baseConfig, env, defaultConfig) => {
+
+  // We extend the default config by
+  const copyWebpackPlugin = new CopyWebpackPlugin([
+    {
+      from: path.resolve(__dirname, '../node_modules/stencil-components-spike/dist'),
+      to: path.posix.join('static', 'stencil-components-spike'),
+      ignore: ['.*']
+    },
+    {
+      from: path.resolve(__dirname, '../src/assets'),
+      to: path.posix.join('static', 'assets'),
+      ignore: ['.*']
+    }
+  ]);
+
+  if (defaultConfig.plugins) {
+    defaultConfig.plugins.push(copyWebpackPlugin);
+  } else {
+    defaultConfig.plugins = [
+      copyWebpackPlugin
+    ];
+  }
+
+  return defaultConfig;
+};
